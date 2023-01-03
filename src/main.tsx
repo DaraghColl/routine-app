f2663d87ea4cb668260c745549caa475f93b9876
Daragh <daraghcoll-@hotmail.com>
2023-01-03 00:15:28 +0000

    feat: use react router

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { RoutineItemsStateProvider } from './state/RoutineItemsState';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RoutineItemsStateProvider>
+      <RouterProvider router={router} />
+    </RoutineItemsStateProvider>
   </React.StrictMode>,
 );
