cc3f6bb36963a1b99f719349285f1f6cdb0009bc
Daragh <daraghcoll-@hotmail.com>
2023-01-04 01:15:18 +0000

    fix: remove blue bg color

--- src/components/RoutineItem/RoutineItem.tsx
+++ src/components/RoutineItem/RoutineItem.tsx
@@ -28,7 +28,7 @@ const RoutineItem: FC<RoutineItemProps> = (props) => {
       <div className="relative mb-4 flex cursor-pointer items-center justify-between rounded-lg bg-white px-4 py-4 align-middle tracking-wide text-slate-800 ring-1 ring-slate-900/5 dark:bg-gray-900 dark:text-white">
         <div>
           <div
-            className={`absolute left-0 top-0 h-full w-2 rounded-l-md bg-blue-500 ${
+            className={`absolute left-0 top-0 h-full w-2 rounded-l-md ${
               statusColorMap[item.status]
             }`}
           ></div>
