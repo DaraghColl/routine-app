53d0a02d4dc132cd52efa1c59f4febb38534f7e7
Daragh <daraghcoll-@hotmail.com>
2023-01-06 01:26:22 +0000

    feat: remove elipsis and add time

--- src/components/RoutineItem/RoutineItem.tsx
+++ src/components/RoutineItem/RoutineItem.tsx
@@ -24,29 +24,29 @@ const RoutineItem: FC<RoutineItemProps> = (props) => {
   };
 
   return (
-    <Reorder.Item key={item.id} value={item}>
-      <div className="relative mb-4 flex cursor-pointer items-center justify-between rounded-lg bg-white px-4 py-4 align-middle tracking-wide text-slate-800 ring-1 ring-slate-900/5 dark:bg-gray-900 dark:text-white">
-        <div>
+    <Reorder.Item key={item.id} value={item} onClick={() => onItemSelect(item.id)}>
+      <div className="relative mb-4 flex cursor-pointer items-center justify-between rounded-lg  bg-white px-4 py-4 align-middle tracking-wide shadow-md ring-0 dark:border-slate-500 dark:bg-slate-800">
+        <div className="w-full">
           <div
             className={`absolute left-0 top-0 h-full w-2 rounded-l-md ${
               statusColorMap[item.status]
             }`}
           ></div>
-
-          <h3 className="text-base font-semibold tracking-wider text-black dark:text-white">
-            {item.title}
-          </h3>
+          <div className="flex items-center gap-2">
+            <h3 className="text-lg font-semibold tracking-wider text-slate-900 dark:text-slate-50">
+              {item.title}
+            </h3>
+            <span className="text-2xl">{item.icon}</span>
+          </div>
           {item.subTitle && (
-            <p className="mt-2 text-sm font-normal   text-slate-500 dark:text-slate-400">
+            <p className="truncate text-sm leading-6 text-slate-500 dark:text-slate-400">
               {item.subTitle}
             </p>
           )}
         </div>
-        <EllipsisVerticalIcon
-          className="ml-2 -mr-1 h-5 w-5 text-violet-200 hover:text-violet-100"
-          aria-hidden="true"
-          onClick={() => onItemSelect(item.id)}
-        />
+        <p className="mt-2 min-w-[75px] border-b-2 py-2 text-center text-slate-500 dark:text-slate-400">
+          {item.time}
+        </p>
       </div>
     </Reorder.Item>
   );
