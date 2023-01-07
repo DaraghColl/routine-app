163b910ae4e932069f220efc18f9214f76006abf
Daragh <daraghcoll-@hotmail.com>
2023-01-07 01:38:39 +0000

    feat: add new item component

---
+++ src/components/AddRoutineItem/AddRoutineItem.tsx
@@ -0,0 +1,30 @@
+import { FC, useContext } from 'react';
+import { RoutineItemsStateContext } from '../../state/RoutineItemsState';
+import { PlusIcon } from '@heroicons/react/24/outline';
+
+const AddRoutineItem: FC = () => {
+  const { addRoutineItem } = useContext(RoutineItemsStateContext);
+
+  return (
+    <div className="mb-4 flex w-full items-center justify-end">
+      <button
+        className="text-md flex items-center rounded-md border border-transparent bg-slate-800 px-4 py-2 font-medium text-white focus:outline-none dark:bg-white dark:text-slate-800"
+        onClick={() =>
+          addRoutineItem({
+            id: 4,
+            title: 'Add new',
+            icon: '🏃‍♂️',
+            subTitle: 'Distance: 5km',
+            time: '09:00',
+            status: 'todo',
+          })
+        }
+      >
+        <PlusIcon className="mr-2 h-4 w-4 text-white dark:text-slate-800" />
+        New Item
+      </button>
+    </div>
+  );
+};
+
+export { AddRoutineItem };
