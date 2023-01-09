cf0ef2c20c8d5d37a4f16bdae16a715c09fced07
Daragh <daraghcoll-@hotmail.com>
2023-01-09 02:16:46 +0000

    feat: initial add item form

--- src/components/AddRoutineItem/AddRoutineItem.tsx
+++ src/components/AddRoutineItem/AddRoutineItem.tsx
@@ -1,28 +1,103 @@
-import { FC, useContext } from 'react';
+import { FC, Fragment, useContext, useState } from 'react';
 import { RoutineItemsStateContext } from '../../state/RoutineItemsState';
 import { PlusIcon } from '@heroicons/react/24/outline';
+import { Dialog, Transition } from '@headlessui/react';
+import { useForm } from 'react-hook-form';
+import { InputItem } from './InputItem';
 
 const AddRoutineItem: FC = () => {
   const { addRoutineItem } = useContext(RoutineItemsStateContext);
+  const [isOpen, setIsOpen] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+    reset,
+  } = useForm();
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
+  const onAddNewItem = () => {
+    setIsOpen(true);
+  };
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+    addRoutineItem({
+      id: 4,
+      title: data.title,
+      icon: data.icon,
+      subTitle: data.subTitle,
+      time: data.time,
+      status: 'todo',
+    });
+
+    reset();
+    closeModal();
+  };
 
   return (
     <div className="mb-4 flex w-full items-center justify-end">
       <button
         className="text-md flex items-center rounded-md border border-transparent bg-slate-800 px-4 py-2 font-medium text-white focus:outline-none"
-        onClick={() =>
-          addRoutineItem({
-            id: 4,
-            title: 'Add new',
-            icon: '🏃‍♂️',
-            subTitle: 'Distance: 5km',
-            time: '09:00',
-            status: 'todo',
-          })
-        }
+        onClick={() => onAddNewItem()}
       >
         <PlusIcon className="mr-2 h-4 w-4 text-white" />
         New Item
       </button>
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={closeModal}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-25" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="absolute bottom-0 h-auto w-11/12 transform overflow-hidden rounded-t-lg rounded-b-none bg-white p-6 text-left align-middle shadow-xl transition-all dark:bg-slate-800 dark:text-white">
+                  <div className="mb-6 md:flex md:items-center">
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                      <div className="md:w-2/3">
+                        <InputItem register={register} name="title" title="Name" />
+                        <InputItem register={register} name="icon" title="Icon" />
+                        <InputItem register={register} name="subTitle" title="Sub title" />
+                        <InputItem register={register} name="time" title="Time" />
+
+                        <button
+                          className="text-md mt-2 flex w-full items-center justify-around rounded-md border border-transparent bg-slate-800 px-4 py-2 font-medium text-white focus:outline-none 
+                          dark:border-white"
+                          type="submit"
+                        >
+                          Submit
+                        </button>
+                      </div>
+                    </form>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
     </div>
   );
 };
---
+++ src/components/AddRoutineItem/InputItem.tsx
@@ -0,0 +1,27 @@
+import { FC } from 'react';
+
+interface InputItemProps {
+  register: any;
+  name: string;
+  title: string;
+}
+
+const InputItem: FC<InputItemProps> = ({ register, name, title, ...rest }) => {
+  return (
+    <div>
+      <label htmlFor={name} className="text-sm">
+        {title}
+      </label>
+      <input
+        className="mb-2 w-full appearance-none rounded border-2 border-slate-100 bg-slate-100 py-2 px-4 leading-tight text-gray-700 focus:outline-none dark:border-slate-700 dark:bg-slate-700
+        dark:text-white"
+        id="inline-full-name"
+        type="text"
+        defaultValue=""
+        {...register(name)}
+      />
+    </div>
+  );
+};
+
+export { InputItem };
